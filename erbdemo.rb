cc7d7703e87f5c61dd10c781aa27b4ed01c2638f
smartdev1010 <shvab.serhii86@gmail.com>
2023-05-24 13:26:37 +0300

    embeded ruby added

---
+++ erbdemo.rb
@@ -0,0 +1,13 @@
+<% page_title="Demonstration of ERB" %>
+<% salutation ="Dear programmer," %>
+
+<html>
+  <head>
+    <title><%= page_title %></title>
+  </head>
+  <body>
+    <h1>
+      <%= salutation %>
+    </h1>
+  </body>
+</html>
